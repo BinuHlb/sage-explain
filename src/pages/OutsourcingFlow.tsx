8562beb5f8f9584237eccfdd390ef1166edfdae3
binuboston <binu.boston66@gmail.com>
2025-05-23 08:34:12 +0530

    title

--- src/pages/OutsourcingFlow.tsx
+++ src/pages/OutsourcingFlow.tsx
@@ -10,7 +10,7 @@ const OutsourcingFlow: React.FC = () => {
       <div className="flex h-screen bg-gray-50">
         <Sidebar />
         <div className="flex-1 flex flex-col overflow-hidden">
-          <Header title="Outsourcing Flow" />
+          <Header title="JOF Tracking" />
           <main className="flex-1 overflow-y-auto p-4 md:p-6">
             <OutsourcingDashboard />
           </main>
